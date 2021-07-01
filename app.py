86d65e3b3f10ddcd55d35105d392169fdfdd53a0
prabodhw96 <prabodhwankhede1996@gmail.com>
2021-07-01 23:27:28 +0530

    speed up

--- app.py
+++ app.py
@@ -27,59 +27,19 @@ def fetch_data(stocks):
 		st.stop()
 	return data
 
-def coskew(df):
-	num = len(df.columns)
-	mtx = np.zeros(shape = (len(df), num**2))
-	v = df.values
-	means = v.mean(0,keepdims=True)
-	v1 = (v-means).T
-
-	for i in range(num):
-		for j in range(num):
-			vals = v1[i]*v1[j]
-			mtx[:,(i*num)+j] = vals/float((len(df)-1)*df.iloc[:,i].std()*df.iloc[:,j].std())
-
-	m3 = np.dot(v1,mtx)
-
-	for i in range(num**2):
-		use = i%num
-		m3[:,i] = m3[:,i]/float(df.iloc[:,use].std())
-
-	return m3
-
-def cokurt(df):
-	num = len(df.columns)
-	mtx1 = np.zeros(shape = (len(df), num**2))
-	mtx2 = np.zeros(shape = (len(df), num**3))
-	v = df.values
-	means = v.mean(0,keepdims=True)
-	v1 = (v-means).T
-
-	for k in range(num):
-		for i in range(num):
-			for j in range(num):
-				vals = v1[i]*v1[j]*v1[k]
-				mtx2[:,(k*(num**2))+(i*num)+j] = vals/float((len(df)-1)*df.iloc[:,i].std()*df.iloc[:,j].std()*df.iloc[:,k].std())
-
-	m4 = np.dot(v1,mtx2)
-
-	for i in range(num**3):
-		use = i%num
-		m4[:,i] = m4[:,i]/float(df.iloc[:,use].std())
-
-	return m4
-
 def portfolio_annualized_performance(weights, mean_returns, cov_matrix):
 	returns = np.sum(mean_returns*weights)*252
 	std = np.sqrt(np.dot(weights.T, np.dot(cov_matrix, weights)))*np.sqrt(252)
 	return std, returns
 
 def neg_prob_sharpe_ratio(weights, mean_returns, log_returns, cov_matrix, risk_free_rate, sharpe_ratio_benchmark):
-	p_var, p_ret = portfolio_annualized_performance(weights, mean_returns, cov_matrix)
-	sharpe_ratio = (p_ret - risk_free_rate)/p_var
-	sharpe_ratio_stdev = np.sqrt((1+(0.5*sharpe_ratio**2) - (coskew(log_returns).mean()*sharpe_ratio) + (((cokurt(log_returns).mean()-3)/4)*sharpe_ratio**2))/(len(mean_returns)-1))
-	prob_sharpe_ratio = stats.norm.cdf((sharpe_ratio - sharpe_ratio_benchmark)/sharpe_ratio_stdev)
-	return -1*prob_sharpe_ratio
+    p_var, p_ret = portfolio_annualized_performance(weights, mean_returns, cov_matrix)
+    sharpe_ratio = (p_ret - risk_free_rate)/p_var
+    coskew = pd.Series(stats.skew(mean_returns), index=mean_returns.index).mean()
+    cokurt = pd.Series(stats.kurtosis(mean_returns, fisher=False), index=mean_returns.index).mean()    
+    sharpe_ratio_stdev = np.sqrt((1+(0.5*sharpe_ratio**2) - coskew*sharpe_ratio) + (((cokurt-3)/4)*sharpe_ratio**2))/(len(mean_returns)-1)
+    prob_sharpe_ratio = stats.norm.cdf((sharpe_ratio - sharpe_ratio_benchmark)/sharpe_ratio_stdev)
+    return -1*prob_sharpe_ratio
 
 def max_sharpe_ratio(mean_returns, log_returns, cov_matrix, risk_free_rate, sharpe_ratio_benchmark=0.0):
 	num_assets = len(mean_returns)
