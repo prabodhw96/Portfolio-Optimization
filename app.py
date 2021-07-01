d0edba561c3248ab1e44d23d9fe3e04d26917341
prabodhw96 <prabodhwankhede1996@gmail.com>
2021-07-01 22:38:47 +0530

    Incorporate skewness and kurtosis

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ plt.style.use("ggplot")
 import pandas_datareader.data as web
 from datetime import date, timedelta
 import plotly.graph_objects as go
+from scipy import stats
 import scipy.optimize as sco
 import streamlit as st
 
@@ -26,22 +27,67 @@ def fetch_data(stocks):
 		st.stop()
 	return data
 
+def coskew(df):
+	num = len(df.columns)
+	mtx = np.zeros(shape = (len(df), num**2))
+	v = df.values
+	means = v.mean(0,keepdims=True)
+	v1 = (v-means).T
+
+	for i in range(num):
+		for j in range(num):
+			vals = v1[i]*v1[j]
+			mtx[:,(i*num)+j] = vals/float((len(df)-1)*df.iloc[:,i].std()*df.iloc[:,j].std())
+
+	m3 = np.dot(v1,mtx)
+
+	for i in range(num**2):
+		use = i%num
+		m3[:,i] = m3[:,i]/float(df.iloc[:,use].std())
+
+	return m3
+
+def cokurt(df):
+	num = len(df.columns)
+	mtx1 = np.zeros(shape = (len(df), num**2))
+	mtx2 = np.zeros(shape = (len(df), num**3))
+	v = df.values
+	means = v.mean(0,keepdims=True)
+	v1 = (v-means).T
+
+	for k in range(num):
+		for i in range(num):
+			for j in range(num):
+				vals = v1[i]*v1[j]*v1[k]
+				mtx2[:,(k*(num**2))+(i*num)+j] = vals/float((len(df)-1)*df.iloc[:,i].std()*df.iloc[:,j].std()*df.iloc[:,k].std())
+
+	m4 = np.dot(v1,mtx2)
+
+	for i in range(num**3):
+		use = i%num
+		m4[:,i] = m4[:,i]/float(df.iloc[:,use].std())
+
+	return m4
+
 def portfolio_annualized_performance(weights, mean_returns, cov_matrix):
 	returns = np.sum(mean_returns*weights)*252
 	std = np.sqrt(np.dot(weights.T, np.dot(cov_matrix, weights)))*np.sqrt(252)
 	return std, returns
 
-def neg_sharpe_ratio(weights, mean_returns, cov_matrix, risk_free_rate):
+def neg_prob_sharpe_ratio(weights, mean_returns, log_returns, cov_matrix, risk_free_rate, sharpe_ratio_benchmark):
 	p_var, p_ret = portfolio_annualized_performance(weights, mean_returns, cov_matrix)
-	return -(p_ret - risk_free_rate)/p_var
+	sharpe_ratio = (p_ret - risk_free_rate)/p_var
+	sharpe_ratio_stdev = np.sqrt((1+(0.5*sharpe_ratio**2) - (coskew(log_returns).mean()*sharpe_ratio) + (((cokurt(log_returns).mean()-3)/4)*sharpe_ratio**2))/(len(mean_returns)-1))
+	prob_sharpe_ratio = stats.norm.cdf((sharpe_ratio - sharpe_ratio_benchmark)/sharpe_ratio_stdev)
+	return -1*prob_sharpe_ratio
 
-def max_sharpe_ratio(mean_returns, cov_matrix, risk_free_rate):
+def max_sharpe_ratio(mean_returns, log_returns, cov_matrix, risk_free_rate, sharpe_ratio_benchmark=0.0):
 	num_assets = len(mean_returns)
-	args = (mean_returns, cov_matrix, risk_free_rate)
+	args = (mean_returns, log_returns, cov_matrix, risk_free_rate, sharpe_ratio_benchmark)
 	constraints = ({"type": "eq", "fun": lambda x: np.sum(x) - 1})
 	bound = (0.0,1.0)
 	bounds = tuple(bound for asset in range(num_assets))
-	result = sco.minimize(neg_sharpe_ratio, num_assets*[1./num_assets,], args=args, method="SLSQP", bounds=bounds, constraints=constraints)
+	result = sco.minimize(neg_prob_sharpe_ratio, num_assets*[1./num_assets,], args=args, method="SLSQP", bounds=bounds, constraints=constraints)
 	return result
 
 def portfolio_volatility(weights, mean_returns, cov_matrix):
@@ -139,11 +185,13 @@ if st.sidebar.checkbox("Daily Returns", False):
 mean_returns = returns.mean()
 cov_matrix = returns.cov()
 risk_free_rate = 0.0178
+log_returns = np.log(data/data.shift(1))
+log_returns = log_returns.dropna()
 
 log_ret = np.log(data/data.shift(1))
 log_ret = log_ret.dropna()
 
-max_sharpe = max_sharpe_ratio(mean_returns, cov_matrix, risk_free_rate)
+max_sharpe = max_sharpe_ratio(mean_returns, log_returns, cov_matrix, risk_free_rate, sharpe_ratio_benchmark=2.0)
 sdp, rp = portfolio_annualized_performance(max_sharpe["x"], mean_returns, cov_matrix)
 max_sharpe_allocation = pd.DataFrame(max_sharpe["x"], index=data.columns, columns=["allocation"])
 max_sharpe_allocation["allocation"] = [round(i*100, 2)for i in max_sharpe_allocation["allocation"]]
